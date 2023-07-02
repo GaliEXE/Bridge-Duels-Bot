2030583f8ded3c31c2fcadb17aea62bd348ab1c8
Gryphon Miller <95644173+GaliEXE@users.noreply.github.com>
2023-07-02 14:05:40 -0500

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { token } = process.env;
+const { token } = process.env
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 
@@ -7,24 +7,12 @@ const client = new Client({ intents: GatewayIntentBits.Guilds });
 client.commands = new Collection();
 client.commandArray = [];
 
-const functionFolders = fs.readdirSync('./src/functions');
+const functionFolders = fs.readdirSync(`./src/functions`);
 for (const folder of functionFolders) {
-  const functionFiles = fs.readdirSync(`./src/functions/${folder}`).filter(file => file.endsWith('.js'));
-  for (const file of functionFiles) require(`./functions/${folder}/${file}`)(client);
+    const functionFiles = fs.readdirSync(`./src/functions/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of functionFiles) require(`./functions/${folder}/${file}`)(client);
 }
 
 client.handleEvents();
-client.ws.on('INTERACTION_CREATE', async (interaction) => {
-  // Handle interactions from slash commands here...
-});
-
-client.login(token)
-  .then(() => {
-    console.log('Logged in successfully');
-    console.log('Bot is now listening on 0.0.0.0');
-  })
-  .catch((error) => {
-    console.error('Error logging in:', error);
-  });
-
+client.login(token);
 client.handleCommands();
